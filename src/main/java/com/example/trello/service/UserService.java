5330c6d3f87b85faffc8a12b4a4d536274cef585
Hieu <Thanglong@gmail.com>
2022-04-30 20:21:56 +0700

    add db

--- src/main/java/com/example/trello/service/UserService.java
+++ src/main/java/com/example/trello/service/UserService.java
@@ -1,14 +1,27 @@
 package com.example.trello.service;
 
-import com.example.trello.dto.UserDto;
+import com.example.trello.dto.MessageDto;
+import com.example.trello.model.User;
 
 import java.util.List;
 
 public interface UserService {
 
-    UserDto update(UserDto userDto);
+    public List<User> getAllUser();
 
-    void delete(long id);
+    public User findById(Long id);
 
-    List<UserDto> display();
+    public User findUserByUserName(String userName);
+
+    User findByEmail(String email);
+
+    List<User> findUserByRole(String role);
+
+    User save(User user);
+
+    MessageDto updateUser(User user);
+
+    boolean activeFirstTime(String userName, String password);
+
+    boolean detective(Long userId);
 }
