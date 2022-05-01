60588e4a3b97b4230de9b39be2f393f3215c9b42
Hieu <Thanglong@gmail.com>
2022-05-01 12:34:08 +0700

    update swager

--- src/main/java/com/example/trello/config/SwaggerConfiguration.java
+++ src/main/java/com/example/trello/config/SwaggerConfiguration.java
@@ -4,8 +4,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -22,19 +24,30 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 public class SwaggerConfiguration extends WebMvcConfigurationSupport {
 
     @Bean
-    public Docket api() {
+    public Docket productApi() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-//      .apis(RequestHandlerSelectors.any())
                 .apis(RequestHandlerSelectors.basePackage("com.example.trello"))
                 .paths(PathSelectors.any())
+                .build()
+                .apiInfo(metaData());
+    }
+    private ApiInfo metaData() {
+        return new ApiInfoBuilder()
+                .title("Spring Boot Swagger App")
+                .description("\"Spring Boot Swagger Server App\"")
+                .version("1.0.0")
+                .license("Apache License Version 2.0")
+                .licenseUrl("https://www.apache.org/licenses/LICENSE-2.0\"")
                 .build();
     }
 
+
     @Override
-    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("swagger-ui.html")
                 .addResourceLocations("classpath:/META-INF/resources/");
+
         registry.addResourceHandler("/webjars/**")
                 .addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
