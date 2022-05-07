2489d6f55d4bc71edc4d95891627cc5869d4a327
Hieu <Thanglong@gmail.com>
2022-05-07 21:58:16 +0700

    update

--- src/main/java/com/example/trello/dto/MessageDto.java
+++ src/main/java/com/example/trello/dto/MessageDto.java
@@ -1,8 +1,12 @@
 package com.example.trello.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class MessageDto {
     String message;
     Boolean obj;
