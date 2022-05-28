63ab820fde15239d3eeae0c38c5b4715ac7acdb0
Hieu <Thanglong@gmail.com>
2022-05-28 18:35:44 +0700

    cors

--- src/main/java/com/example/trello/TrelloApplication.java
+++ src/main/java/com/example/trello/TrelloApplication.java
@@ -4,6 +4,9 @@ import com.example.trello.service.UploadService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import javax.annotation.Resource;
 
@@ -21,6 +24,16 @@ public class TrelloApplication implements CommandLineRunner {
     @Override
     public void run(String... arg) {
         uploadService.init();
+    }
+
+    @Bean
+    public WebMvcConfigurer configurer(){
+        return new WebMvcConfigurer(){
+            @Override
+            public void addCorsMappings(CorsRegistry reg){
+                reg.addMapping("/*").allowedOrigins("*");
+            }
+        };
     }
 
 }
